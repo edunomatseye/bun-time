cc612274fcb16646118ba8734436a38eba5a4252
Edun Omatseye <edunomatseye@yahoo.com>
2023-09-28 12:16:18 +0100

    changed .text() into .json() deserialization

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 console.log("Hello via Bun!");
 
-const content = await Bun.file("./package.json").text();
+const content = await Bun.file("./package.json").json();
 console.log(content);
 await Bun.write("./content.json", content);
